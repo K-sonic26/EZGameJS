854b41930fcc75fb3ab38da7a74d397acfa39ee0
pooki12 <maysuncampos@yahoo.com>
2022-11-04 21:28:59 -0600

    Package Manager

---
+++ src/package_manager.sh
@@ -0,0 +1,11 @@
+echo "EZGameJS | Package Manager"
+echo ""
+echo "Make a folder for your packages"
+read fold
+mkdir fold
+echo ""
+echo "done!"
+echo ""
+echo "Add a package from github"
+read pro
+git clone pro
