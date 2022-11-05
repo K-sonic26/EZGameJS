7595ee2e5a7908b2ce73c86dc68c00823917cf69
pooki12 <maysuncampos@yahoo.com>
2022-11-04 21:56:11 -0600

    Credit Comments, Loops, & Compiler Check Loop Added

--- src/EZGameJS/main.c
+++ src/EZGameJS/main.c
@@ -1,3 +1,14 @@
+/**********************************************************
+* Written By: LoonCadets                                  *
+* Written In: The C Programming Language                  *
+* Written By: Pooki12                                     *
+*                                                         *
+* This File Is A Part Of EZGameJS Project                 *
+* Make Your Games Easier                                  *
+*                                                         *
+* Copyright LoonCadets (C) 2022 All Rights Reserved       *
+***********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -10,9 +21,15 @@ int main(int argc, char *argv) {
   
   if (account = true) {
     // do nothing
+  } else if (account = false) {
+    printf("Please Change Account Variable To True\n");
   } else {
     return NULL;
-    printf("Please Change Account Variable To True\n");
+  }
+  
+  // Compiler loop
+  
+  while true() {
   }
   
   return 0;
