af5fae6665cc68d1ec203e32e3e837e427965fac
pooki12 <maysuncampos@yahoo.com>
2022-11-04 21:42:33 -0600

    Create ezGame.h

---
+++ src/EZGameJS/ezGame.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <math.h>
+
+typedef shortcuts(keywords) {
+}
+
+typedef makeExe(file, make, compiler) {
+}
+
+typedef compile(check) {
+}
+
+typedef test(testCompile, exit)  {
+}
+
+typedef sprites(model, anim, movement) {
+}
+
+typedef ThreeD(render, complete_3d) {
+}
+
+typedef TwoD(render_2dimensionals) {
+}
